3750a4171d30f3f6dfbdb8e9209eac9a20e8a2db
Mathews A George <121181320+mathewsageorge@users.noreply.github.com>
2024-01-31 00:36:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,73 @@
-const $status=document.getElementById("status")
-const $log=document.getElementById("log")
+const $status = document.getElementById("status");
+const $log = document.getElementById("log");
 
-const currentTime=()=>{
-    return new Date().toString().slice(0,-31)
+const currentTime = () => {
+  return new Date().toString().slice(0, -31);
+};
+
+let currentStatus = "?";
+let checkedSerialNumbers = new Set();
+
+const handleNewRecord = (serialNumber, logData, time) => {
+  const $record = document.createElement("div");
+  $record.innerHTML = `\n${serialNumber} - <b>${logData}</b> - ${time}`;
+  $log.appendChild($record);
+};
+
+if (!window.NDEFReader) {
+  $status.innerHTML = "<h4>NFC Unsupported!</h4>";
 }
 
-let currrentStatus="?"
+const activateNFC = () => {
+  const ndef = new NDEFReader();
 
-const handleNewRecord=(serialNumber,logData,time)=>{
-    $record=document.createElement("div")
-    $record.innerHTML=`\n${serialNumber} - <b>${logData}</b> - ${time}`
-    $log.appendChild($record)
-}
-
-if(!window.NDEFReader){
-    $status.innerHTML="<h4>Unsupported device!</h4>"
-}
-
-const activateNFC=()=>{
-    const ndef = new NDEFReader()
-
-    ndef.scan().then(()=>{
-        $status.innerHTML="<h4>Bring a NFC tag towards the back of your phone...</h4>"
+  ndef
+    .scan()
+    .then(() => {
+      $status.innerHTML =
+        "<h4>Bring an NFC tag towards the back of your phone...</h4>";
     })
-    .catch(err=>{
-        console.log("Scan Error:",err)
-        alert(err)
-    })
+    .catch((err) => {
+      console.log("Scan Error:", err);
+      alert(err);
+    });
 
-    ndef.onreadingerror = (e) => {
-        $status.innerHTML="<h4>Read Error</h4>"+currentTime()
-        console.log(e);
-    };
+  ndef.onreadingerror = (e) => {
+    $status.innerHTML = "<h4>Read Error</h4>" + currentTime();
+    console.log(e);
+  };
 
-    ndef.onreading = (e) => {
-        let time=currentTime()
-        let {serialNumber}=e
-        $status.innerHTML=`<h4>Last Read</h4>${serialNumber}<br>${currentTime()}`
-        handleNewRecord(serialNumber,currrentStatus,time)
-        console.log(e);
-    };
-}
+  ndef.onreading = (e) => {
+    const time = currentTime();
+    const { serialNumber } = e;
 
-document.getElementById("check-in").onchange=(e)=>{
-    e.target.checked && (currrentStatus="in")
-}
-document.getElementById("check-out").onchange=(e)=>{
-    e.target.checked && (currrentStatus="out")
-}
-document.getElementById("start-btn").onclick=(e)=>{
-    activateNFC()
-}
+    if (checkedSerialNumbers.has(serialNumber)) {
+      $status.innerHTML = `<h4>Duplicate Check</h4>${serialNumber} - ${
+        currentStatus === "in" ? "Check In" : "Check Out"
+      } already recorded at ${time}`;
+      return;
+    }
 
+    checkedSerialNumbers.add(serialNumber);
+
+    $status.innerHTML = `<h4>Last Read</h4>${serialNumber}<br>${currentTime()}`;
+    handleNewRecord(serialNumber, currentStatus, time);
+    console.log(e);
+  };
+};
+
+document.getElementById("check-in").onchange = (e) => {
+  if (e.target.checked) {
+    currentStatus = "in";
+  }
+};
+
+document.getElementById("check-out").onchange = (e) => {
+  if (e.target.checked) {
+    currentStatus = "out";
+  }
+};
+
+document.getElementById("start-btn").onclick = (e) => {
+  activateNFC();
+};
